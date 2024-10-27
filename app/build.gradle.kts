31292bf543486b676c5d379546a21d841702dce7
r_malapitan <r_malapitan@fanshawec.ca>
2024-10-26 23:46:50 -0400

    Adding the firebase SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -57,5 +58,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
 }
